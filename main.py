ab5cab30f6f32b741a7fc9337d08bba2622f8f14
yaghsha <145968112+yaghsha@users.noreply.github.com>
2025-02-04 13:17:13 +0500

    turtle cross game

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
-from question_model import Question
-from data import question_data
-from quiz_brain import QuizBrain
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
 
-question_bank = []
-for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
-    new_question = Question(question_text, question_answer)
-    question_bank.append(new_question)
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
 
-quiz = QuizBrain(question_bank)
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
 
-while quiz.still_has_questions():
-    quiz.next_question()
+screen.listen()
+screen.onkey(player.go_up,"Up")
 
-print("You've completed the quiz")
-print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+    # Detect collision with car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+            scoreboard.game_over()
+
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+screen.exitonclick()
